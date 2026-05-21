3b34ef0599f5df61cbd4b995a98cb9312800780c
Ryan Teh <srwog+github@live.com>
2026-05-21 20:13:58 +0800

    remove error pages

--- Chatbot/Program.cs
+++ Chatbot/Program.cs
@@ -41,16 +41,6 @@ builder.Services.AddHttpClient<WahaSendTextClient>((serviceProvider, httpClient)
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-
 app.MapWahaWebhookEndpoints();
 
 app.Run();
